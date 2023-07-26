0424b826b85d4801211e2dbbe583a1c778b0c600
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2023-07-26 18:33:51 +0000

    second commit

---
+++ basics/data_types.js
@@ -0,0 +1,8 @@
+let name="shabhankar"
+let age=19
+let yes=false
+//null =>standalone value
+//undefined
+//symbol =>unique
+console.log(typeof (name))
+
---
+++ basics/variable.js
@@ -0,0 +1,7 @@
+const accountId=24897;
+let accountEmail="shubhankar@email.com"
+var accountCity="Pune"
+ /* we cannot change the value of the variable created by const
+  var have scope problem that's why let have came*/
+
+console.table([accountId,accountEmail,accountCity])
