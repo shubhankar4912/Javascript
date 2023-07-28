3f9bc120aae23480d60d4b84e03107466a62a7fd
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2023-07-28 10:55:24 +0000

    maths

---
+++ basics/04_Strings.js
@@ -0,0 +1,8 @@
+ let name ="shubhankar"
+ let age=19
+ console.log(`hello my name is ${name} and my age is ${age}`); 
+ let nickname=new String('Shubh');
+
+
+console.log(name.substring(0,4));
+console.log(nickname.split('S'));
