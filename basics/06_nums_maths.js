68c887918bf2eca62dcc783d641d2cb95fccb7ed
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2023-07-28 10:53:46 +0000

    strings

---
+++ basics/06_nums_maths.js
@@ -0,0 +1,19 @@
+// let score =400
+// console.log(score);
+// let num=new Number(400);
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toFixed(2));
+// const m=1000000
+// console.log(m.toLocaleString('en-IN'));
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.9));
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
