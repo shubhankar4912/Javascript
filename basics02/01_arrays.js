bc40b740e68cb3d40aa71067cd97ce3c04b67eb0
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2023-07-29 08:50:57 +0000

    arrays

---
+++ basics02/01_arrays.js
@@ -0,0 +1,18 @@
+let arr=new Array(1,2,3,4,5)
+
+// console.log(arr);
+// arr.pop()
+
+// arr.sort()
+// console.log(arr);
+// arr.unshift(5)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// console.log(arr.indexOf(5));
+let arr1=arr.slice(1,3)
+console.log("A",arr);
+console.log(arr1);
+let arr2=arr.splice(1,3)
+console.log("B",arr);
+console.log(arr2);
