3d27d71974959a718ba49b2a73f69bc629461c5b
shubhankar4912 <97121506+shubhankar4912@users.noreply.github.com>
2023-07-29 07:21:13 +0000

    dates

---
+++ basics/07_dates.js
@@ -0,0 +1,18 @@
+// let mydate=new Date
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toJSON());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleTimeString());
+// console.log(mydate.toTimeString());
+// console.log(mydate.toUTCString());
+// console.log(typeof mydate);
+
+let date=new Date(2023,1,23)
+// console.log(date.toDateString());
+let timestamp=Date.now()
+// console.log(timestamp);
+// console.log(date.getTime());
+// console.log(Math.floor(Date.now()/1000));
